20d017eaf7184810a9389cf2fc08ab33cd9316f2
treskunov.boris <treskunov.boris@c7e220af-e997-f780-5162-34518a5ba3d8>
2011-06-09 20:09:38 +0000

    1:09 update

--- SafeMode/src/com/teamPrime/sm/BlackListActivity.java
+++ SafeMode/src/com/teamPrime/sm/BlackListActivity.java
@@ -1,68 +1,177 @@
+/** This is the main activity for the blacklist
+ * 
+ * @author Boris Treskunov
+ */
+
+
+//MAKE SURE TO ADD CONTACTS PERMISSION TO MANIFEST IN MAIN VERSION
+
 package com.teamPrime.sm;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.LinkedList;
-import android.app.Activity;
+import java.util.Map;
+
+import android.app.ListActivity;
+import android.database.Cursor;
 import android.os.Bundle;
+import android.provider.ContactsContract;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
 import android.widget.Button;
-import android.widget.EditText;
-import android.widget.ListView;
+import android.widget.SimpleAdapter;
+import android.widget.Toast;
 
-public class BlackListActivity extends Activity {
+public class BlackListActivity extends ListActivity {
     /** Called when the activity is first created. */
 	
-	ListView mBlackList;
-	EditText mTextInput;
-	Button mAddButton;
-	Button mRemoveButton;
+	Button mAddButton;	
 	Button mAddAll;
 	Button mRemoveAll;
-	ArrayAdapter<String> mListAdapter;
-	LinkedList<String> contacts;
+	Button mRemove;
+	ArrayAdapter<String> mArrayAdapterBL;
+	LinkedList<String> blacklistedContacts;
+	AutoCompleteTextView mAutoComplete;
+	ArrayAdapter<String> mArrayAdapterAC;
+	ArrayList<String> addedContacts;
+	ArrayList<Contact> contacts;
+	ArrayList<String> contactNames;
 	
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.black_list);
-      
-        mBlackList = (ListView) findViewById(R.id.blacklist_list);
-        mTextInput = (EditText) findViewById(R.id.blacklist_text);    
-        mAddButton = (Button) findViewById(R.id.add);
-        mRemoveButton = (Button) findViewById(R.id.add);
-        mAddAll = (Button) findViewById(R.id.add);
-        mRemoveAll = (Button) findViewById(R.id.add);
-        contacts = new LinkedList<String>();
-        //contacts.add("test");
-        
-        mListAdapter = new ArrayAdapter<String>(this, R.layout.black_list, contacts);
-        
-        mBlackList.setAdapter(mListAdapter);
-        
+     
+        instantiateVariables();
+        populatePeopleList();    
+ 
         mAddButton.setOnClickListener(new OnClickListener(){    	 	
-    	public void onClick(View v){
-    		mListAdapter.add(mTextInput.getText().toString());
-    		//mBlackList.setAdapter(mListAdapter);
-    	}
-    });
-        
-        mRemoveButton.setOnClickListener(new OnClickListener(){    	 	
         	public void onClick(View v){
-       
-        	}
-        });
-        
+        		String name = mAutoComplete.getText().toString();
+        		if (!contactNames.contains(name))
+        			Toast.makeText(getApplicationContext(), "Incorrect Contact Name!", Toast.LENGTH_LONG).show();
+        		else if (addedContacts.contains(name))
+        			Toast.makeText(getApplicationContext(), "Contact is Already in Blacklist!", Toast.LENGTH_LONG).show();
+        		else{
+        			mArrayAdapterBL.add(name);
+        			addedContacts.add(name);
+        		}
+        		mAutoComplete.setText("");
+        		}
+        	});
+
         mAddAll.setOnClickListener(new OnClickListener(){    	 	
-        	public void onClick(View v){
-       
-        	}
-        });
-        
-        mRemoveAll.setOnClickListener(new OnClickListener(){    	 	
-        	public void onClick(View v){
-       
-        	}
-        });
+        	public void onClick(View v){	
+        		int initialSize = addedContacts.size();
+        		for(Contact contact: contacts) {
+        			if (!addedContacts.contains(contact.getName() + " (" + contact.getNumber() + ")")){
+        				mArrayAdapterBL.add(contact.getName() + " (" + contact.getNumber() + ")");
+        				addedContacts.add(contact.getName() + " (" + contact.getNumber() + ")");
+        				}
+        			if (addedContacts.size()==initialSize) 
+        				Toast.makeText(getApplicationContext(), "All Contacts are Already in Blacklist!", Toast.LENGTH_LONG).show();
+        			}
+        		mAutoComplete.setText("");
+        		}
+        	});
+
+       	mRemoveAll.setOnClickListener(new OnClickListener(){    	 	
+       		public void onClick(View v){
+        		mArrayAdapterBL.clear();
+        		addedContacts.clear();
+        		Toast.makeText(getApplicationContext(), "All Contacts Removed from Blacklist!", Toast.LENGTH_LONG).show();
+        		mAutoComplete.setText("");
+        		}
+        	});
+        	
+       	mRemove.setOnClickListener(new OnClickListener(){    	 	
+       		public void onClick(View v){
+       			String name = mAutoComplete.getText().toString();
+        		if (!addedContacts.contains(name))
+        			Toast.makeText(getApplicationContext(), "Contact is not in Blacklist!", Toast.LENGTH_LONG).show();
+        		else {
+       				mArrayAdapterBL.remove(name);
+       				addedContacts.remove(name);
+       			}
+        		mAutoComplete.setText("");
+        		}
+        	});
+
 }
+    
+    private void instantiateVariables(){
+        mAddButton = (Button) findViewById(R.id.add);
+        mAddAll = (Button) findViewById(R.id.add_all_button);
+        mRemoveAll = (Button) findViewById(R.id.remove_all_button);
+        mRemove = (Button) findViewById(R.id.remove);
+        addedContacts = new ArrayList<String>();
+        blacklistedContacts = new LinkedList<String>();
+        contacts = new ArrayList<Contact>();
+        contactNames = new ArrayList<String>();
+        mArrayAdapterBL = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, blacklistedContacts);
+        this.setListAdapter(mArrayAdapterBL);
+        mArrayAdapterAC = new ArrayAdapter<String>(this,android.R.layout.simple_dropdown_item_1line,contactNames);
+        mAutoComplete = (AutoCompleteTextView)findViewById(R.id.blacklist_text);
+        mAutoComplete.setAdapter(mArrayAdapterAC);
+        mAutoComplete.setThreshold(3); 
+    }
+    
+    public void populatePeopleList() {
+    	contacts.clear();
+    	contactNames.clear();
+    	String phoneNumber = "";
+    	Cursor people = getContentResolver().query(ContactsContract.Contacts.CONTENT_URI, null, null, null, null);
+
+    	while (people.moveToNext()){
+	    	String name = people.getString(people.getColumnIndex(
+	    	ContactsContract.Contacts.DISPLAY_NAME));
+	    	String iD = people.getString(people.getColumnIndex(
+	    	ContactsContract.Contacts._ID));
+	    	String hasPhone = people.getString(people.getColumnIndex(
+	    	ContactsContract.Contacts.HAS_PHONE_NUMBER));
+
+	  		if ((Integer.parseInt(hasPhone) > 0)) {
+	  			Cursor numbers = getContentResolver().query(
+	  					ContactsContract.CommonDataKinds.Phone.CONTENT_URI, null, ContactsContract.CommonDataKinds.Phone.CONTACT_ID +" = "+ iD, null, null);
+	  			while (numbers.moveToNext()) {
+	  				phoneNumber = numbers.getString(numbers.getColumnIndex( ContactsContract.CommonDataKinds.Phone.NUMBER));
+	  				contacts.add(new Contact(iD, name, phoneNumber));
+	  			}
+	  		}
+    	}
+    	people.close();
+    	
+    	for (Contact contact: contacts){
+    		contactNames.add(contact.getName() + " (" + contact.getNumber() + ")");
+    	}
+    	//startManagingCursor(people);
+    }
+    
+/**	@Override 
+ * this was the code for a popupWindow that may be revived one day...
+ * but for now it shall live in comments.
+ *
+	protected void onListItemClick(ListView l, View v, int position, long id) {
+		super.onListItemClick(l, v, position, id);
+		keyword = this.getListAdapter().getItem(position).toString();		
+		LayoutInflater inflater = (LayoutInflater) this.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+	    remove = new PopupWindow(inflater.inflate(R.layout.list, null, false), 200, 200, true);
+	    remove.showAtLocation(this.findViewById(R.id.blacklist_text), Gravity.CENTER, 0, 0);
+	    View pview = inflater.inflate(R.layout.list,(ViewGroup)findViewById(R.layout.main));
+	    Button mRemove=(Button) pview.findViewById(R.id.remove);
+	    
+	    mRemove.setOnClickListener(new OnClickListener(){    	 	
+    		public void onClick(View v){
+    			mArrayAdapter.clear();
+    		}
+    	});
+	    
+
+	}
+ *
+ */
+        
 }
