0eb0bc211a0b5653a2d4f9f40ec8affa1f93391a
treskunov.boris <treskunov.boris@c7e220af-e997-f780-5162-34518a5ba3d8>
2011-06-09 23:51:07 +0000

    4:51 update

--- trunk/SafeMode/src/com/teamPrime/sm/Contact.java
+++ trunk/SafeMode/src/com/teamPrime/sm/Contact.java
@@ -1,15 +1,25 @@
 package com.teamPrime.sm;
 
+import java.util.List;
+
 public class Contact {
 	
 	private String iD;
 	private String name;
-	private String number;
+	private String[] numbers;
 	
-	public Contact (String iD, String name, String number){
+	public Contact (String iD, String name, List<String> numbers){
 		this.iD = iD;
 		this.name = name;
-		this.number = number;
+		String[] nums = new String[numbers.size()];
+		for (int i = 0; i<nums.length; i++){
+			nums[i] = numbers.get(i);
+		}
+		this.numbers = nums;
+	}
+	
+	public boolean match(String test){
+		return test==this.name;
 	}
 	
 	public String getID(){
@@ -20,8 +30,8 @@ public class Contact {
 		return name;
 	}
 	
-	public String getNumber(){
-		return number;
+	public String[] getNumber(){
+		return numbers;
 	}
 
 }
