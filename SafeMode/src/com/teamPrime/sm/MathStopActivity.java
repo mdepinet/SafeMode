e3a601450f5e7f4f6b917955ac45fcba6cef74c4
mikedabomb000@gmail.com <mikedabomb000@gmail.com@c7e220af-e997-f780-5162-34518a5ba3d8>
2011-07-01 21:09:00 +0000

    Moved MathUtils into MathStopActivity since that's now the only place it is used.

--- SafeMode/src/com/teamPrime/sm/MathStopActivity.java
+++ SafeMode/src/com/teamPrime/sm/MathStopActivity.java
@@ -81,4 +81,22 @@ public class MathStopActivity extends Activity{
     	}
     }
     
+    
+    static class MathUtils {
+    	
+    	private static final int[] smallChoice = {3, 4, 6, 7, 8, 9};
+    	private static final int[] largeChoice = {12, 13, 14, 16, 17, 18, 19};
+    	private static Random r = new Random();
+    	
+    	public static int genSmallRandom() {
+    		int index = r.nextInt(smallChoice.length);
+    		return smallChoice[index];
+    	}
+    	
+    	public static int genLargeRandom() {
+    		int index = r.nextInt(largeChoice.length);
+    		return largeChoice[index];
+    	}
+
+    }
 }
