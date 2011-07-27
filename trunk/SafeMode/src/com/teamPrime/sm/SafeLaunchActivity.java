0c797001f9e6174a83c267ffe2455df5f2cf464a
treskunov.boris <treskunov.boris@c7e220af-e997-f780-5162-34518a5ba3d8>
2011-07-27 16:03:54 +0000

    texting added on find me, though it does not use location or contact numbers yet

--- trunk/SafeMode/src/com/teamPrime/sm/SafeLaunchActivity.java
+++ trunk/SafeMode/src/com/teamPrime/sm/SafeLaunchActivity.java
@@ -30,6 +30,7 @@ import android.content.IntentFilter;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.os.CountDownTimer;
+import android.telephony.SmsManager;
 import android.util.Log;
 import android.view.View;
 import android.widget.DatePicker;
@@ -262,10 +263,78 @@ public class SafeLaunchActivity extends Activity {
             	startActivity(new Intent(getApplicationContext(), HistoryActivity.class));
             	break;
             case R.id.dashboard_find_me:
-            	//TODO
+            	try{
+            		//sends message but does not add it to sent messages:
+            		String phoneNumber = "3057789281"; //temporary number
+            		String address = "30837 NE 25th ave"; //temporary address
+            		String message = "I am at " + address;
+            		sendSMS(phoneNumber, message);
+            	}
+            	catch(Exception e){
+            		Log.e("message send error", e.getMessage());
+            		//Toast.makeText(getApplicationContext(), "I'm sorry, tablets do not support texting", Toast.LENGTH_SHORT).show();
+            	} 
             	break;
         }
     }
+    
+    private void sendSMS(String phoneNumber, String message) {        
+    	String SENT = "SMS_SENT";
+        String DELIVERED = "SMS_DELIVERED";
+ 
+        PendingIntent sentPI = PendingIntent.getBroadcast(this, 0,
+            new Intent(SENT), 0);
+ 
+        PendingIntent deliveredPI = PendingIntent.getBroadcast(this, 0,
+            new Intent(DELIVERED), 0);
+ 
+        //---when the SMS has been sent---
+        registerReceiver(new BroadcastReceiver(){
+            @Override
+            public void onReceive(Context arg0, Intent arg1) {
+                switch (getResultCode())
+                {
+                    case Activity.RESULT_OK:
+                        Toast.makeText(getBaseContext(), "Location sent to " + "contact name...", 
+                                Toast.LENGTH_SHORT).show();
+                        break;
+                    case SmsManager.RESULT_ERROR_GENERIC_FAILURE:
+                        Toast.makeText(getBaseContext(), "An error occured, please try again", 
+                                Toast.LENGTH_SHORT).show();
+                        break;
+                    case SmsManager.RESULT_ERROR_NO_SERVICE:
+                        Toast.makeText(getBaseContext(), "No service", 
+                                Toast.LENGTH_SHORT).show();
+                        break;
+                    case SmsManager.RESULT_ERROR_RADIO_OFF:
+                        Toast.makeText(getBaseContext(), "Radio off, unable to send message", 
+                                Toast.LENGTH_SHORT).show();
+                        break;
+                }
+            }
+        }, new IntentFilter(SENT));
+ 
+        //---when the SMS has been delivered---  do we want this?
+        /**registerReceiver(new BroadcastReceiver(){
+            @Override
+            public void onReceive(Context arg0, Intent arg1) {
+                switch (getResultCode())
+                {
+                    case Activity.RESULT_OK:
+                        Toast.makeText(getBaseContext(), "SMS delivered", 
+                                Toast.LENGTH_SHORT).show();
+                        break;
+                    case Activity.RESULT_CANCELED:
+                        Toast.makeText(getBaseContext(), "SMS not delivered", 
+                                Toast.LENGTH_SHORT).show();
+                        break;                        
+                }
+            }
+        }, new IntentFilter(DELIVERED));*/        
+ 
+        SmsManager sms = SmsManager.getDefault();
+        sms.sendTextMessage(phoneNumber, null, message, sentPI, deliveredPI);        
+    }    
     
     private void handleTimes(boolean fromLong, boolean updateDate){
     	if (fromLong && updateDate) handleTimes(true,false); //Before we can update the date, our current values need to be set.
