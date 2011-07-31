2c496dce849a1c7f9b479aea52c52820d6b6d0c6
treskunov.boris <treskunov.boris@c7e220af-e997-f780-5162-34518a5ba3d8>
2011-07-31 02:03:58 +0000

    slight changes made to HistoryActivity to use historyAdapter over adapter in places overlooked in last commit

--- trunk/SafeMode/src/com/teamPrime/sm/history/HistoryItem.java
+++ trunk/SafeMode/src/com/teamPrime/sm/history/HistoryItem.java
@@ -62,7 +62,7 @@ public abstract class HistoryItem implements Serializable, DialogCreator{
 	}
 	
 	public String getTitle(){
-		return "History Item";
+		return "Your History is Currently Empty...";
 	}
 	
 	public String getDate(){
