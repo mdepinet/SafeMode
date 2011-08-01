dae58f8be638c898bd964fc58defcfbbba58538c
treskunov.boris <treskunov.boris@c7e220af-e997-f780-5162-34518a5ba3d8>
2011-08-01 23:38:48 +0000

    date header added, and text entries changed to show phonenumber (should eventually be contact id). date header is commented out for now bc it's pretty buggy

--- SafeMode/src/com/teamPrime/sm/history/DateItem.java
+++ SafeMode/src/com/teamPrime/sm/history/DateItem.java
@@ -3,31 +3,27 @@ package com.teamPrime.sm.history;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
-import android.telephony.PhoneNumberUtils;
-
 import com.teamPrime.sm.HistoryActivity;
 
-public class DefaultFindMeItem extends HistoryItem {
+public class DateItem extends HistoryItem {
 	private static final long serialVersionUID = -361038583952900332L;
 	private Date creationDate = new Date();
-	private String phoneNumber;
 	
-	public DefaultFindMeItem(HistoryActivity activity, String phoneNumber, HistAction defaultAction, HistAction... acts) {
+	public DateItem(HistoryActivity activity, HistAction defaultAction, HistAction... acts) {
 		super(activity, defaultAction, acts);
-		this.phoneNumber = phoneNumber;
 	}
 	
 	public String toString(){
-		return "Find Me Text to " + phoneNumber + "\n" + new SimpleDateFormat(HistoryItem.DATE_FORMAT).format(creationDate);
+		return "This is a date item";
 	}
 	
 	public String getTitle(){
-		return "Find Me Text";
+		return "Date Item";
 	}
 	
 	public String getDescription(){
 		//should be changed to contact name
-		return PhoneNumberUtils.formatNumber(phoneNumber);
+		return "date";
 	}
 	
 	public String getDate(){
