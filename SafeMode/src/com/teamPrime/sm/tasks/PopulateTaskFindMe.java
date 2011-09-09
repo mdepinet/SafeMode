48335f06dc50fb200e1cfc914553d417274b1e93
treskunov.boris <treskunov.boris@c7e220af-e997-f780-5162-34518a5ba3d8>
2011-09-09 05:22:20 +0000

    contacts added to find me, sends location to chosen contact

---
+++ SafeMode/src/com/teamPrime/sm/tasks/PopulateTaskFindMe.java
@@ -0,0 +1,67 @@
+/**
+ * Copyright © 2011 Boris Treskunov
+ * All rights reserved
+ * 
+ * This file is distributed as a part of the SAFEMODE application for
+ * Android devices.  SAFEMODE is distributed under Apache License 2.0
+ * which can be found in full at http://www.apache.org/licenses/LICENSE-2.0
+ */
+
+package com.teamPrime.sm.tasks;
+
+
+import android.app.ProgressDialog;
+import android.os.AsyncTask;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
+
+import com.teamPrime.sm.FindMeActivity;
+import com.teamPrime.sm.R;
+
+public class PopulateTaskFindMe extends AsyncTask<Void, Integer, Void> {
+	private FindMeActivity mActivity;
+	ProgressDialog loading;
+
+    public PopulateTaskFindMe(FindMeActivity activity) {
+        mActivity = activity;
+    }
+
+    // Can change activity this task points to.
+    // e.g. when activity recreated after orientation change.
+    public void setActivity(FindMeActivity activity) {
+        mActivity = activity;
+    }
+
+    // Runs on main thread.
+    @Override
+    protected void onPreExecute() {
+    	loading = ProgressDialog.show(mActivity, "",mActivity.getString(R.string.loading), true); 
+    }
+
+    // Runs on main thread.
+    @Override
+    protected void onProgressUpdate(Integer... count) {
+    }
+
+    // Runs on main thread.
+    @Override
+    protected void onPostExecute(Void result) {
+    	//mActivity.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_SENSOR);
+    	mActivity.populateList();
+    	mActivity.setArrayAdapter(new ArrayAdapter<String>(mActivity,android.R.layout.simple_dropdown_item_1line,mActivity.getContactNames()));
+        try{
+        	loading.dismiss();
+        }
+        catch (Exception e){
+        	
+        }
+    }
+	
+	@Override
+	protected Void doInBackground(Void... arg0) {
+		if(mActivity!=null)
+			mActivity.populatePeopleList();
+		return null;
+	}
+	
+}
