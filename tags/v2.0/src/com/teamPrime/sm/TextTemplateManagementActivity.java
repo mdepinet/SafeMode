f2a9511e4e3e519fedda7a1984de3229c7e8e8c0
mikedabomb000@gmail.com <mikedabomb000@gmail.com@c7e220af-e997-f780-5162-34518a5ba3d8>
2012-05-26 20:26:23 +0000

    Version 2.0 tag

---
+++ tags/v2.0/src/com/teamPrime/sm/TextTemplateManagementActivity.java
@@ -0,0 +1,152 @@
+package com.teamPrime.sm;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.concurrent.ExecutionException;
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.app.ListActivity;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemLongClickListener;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.teamPrime.sm.tasks.TemplateLoadingTask;
+
+public class TextTemplateManagementActivity extends ListActivity {
+	private static final int CONFIRM_DELETE_DIALOG_ID = 0;
+	private static final int ADD_MESSAGE_DIALOG_ID = 1;
+	
+	private ArrayAdapter<String> templatesAdapter;
+	private List<String> templates;
+	private TemplateLoadingTask mTask;
+	private int selectedItem = -1;
+
+	@Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.text_template_management);
+        templates = new LinkedList<String>();
+        mTask = new TemplateLoadingTask(this,templates,TemplateLoadingTask.READ_MODE);
+        mTask.execute();
+        try {
+			mTask.get();
+		} catch (InterruptedException e) {
+			Log.e("SAFEMODE - FindMe Templates","Failed to load templates",e);
+		} catch (ExecutionException e) {
+			Log.e("SAFEMODE - FindMe Templates","Failed to load templates",e);
+		}
+        templatesAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, templates);
+        setListAdapter(templatesAdapter);
+        
+        getListView().setOnItemLongClickListener(new OnItemLongClickListener(){
+			@Override
+			public boolean onItemLongClick(AdapterView<?> arg0, View v, int pos, long id) {
+				return onListItemLongClick(v, pos, id);
+			}
+        });
+	}
+	
+    @Override
+    public void onResume(){
+    	super.onResume();
+    }
+    
+    @Override
+    public void onPause(){
+        super.onPause();
+        if (mTask.getMode() == TemplateLoadingTask.READ_MODE) mTask.cancel(true);
+        mTask = new TemplateLoadingTask(this,templates,TemplateLoadingTask.WRITE_MODE);
+        mTask.execute((Void[]) null);
+    }
+    
+    @Override
+    public void onDestroy(){
+        super.onDestroy();
+    } 
+    
+    protected boolean onListItemLongClick(View v, int position, long id){
+    	selectedItem = position;
+    	showDialog(CONFIRM_DELETE_DIALOG_ID);
+    	return true;
+    }
+    
+    @Override
+	public Dialog onCreateDialog(int id){
+		Dialog d = null;
+		switch(id){
+		case CONFIRM_DELETE_DIALOG_ID:
+			d = new AlertDialog.Builder(this).setMessage(getString(R.string.find_confirm_delete))
+		           .setCancelable(false)
+		           .setPositiveButton(getString(R.string.yes), new DialogInterface.OnClickListener(){
+		        	   public void onClick(DialogInterface dialog, int id) {
+		        		   if (selectedItem >= 0 && selectedItem < templates.size())
+		        			   templatesAdapter.remove(templates.get(selectedItem));
+		        		   selectedItem = -1;
+		        		   dialog.dismiss();
+		        	   }
+		           })
+		           .setNegativeButton(getString(R.string.no), new DialogInterface.OnClickListener() {
+		               public void onClick(DialogInterface dialog, int id) {
+		            	   selectedItem = -1;
+		                   dialog.cancel();
+		               }
+		           }).create();
+			break;
+		case ADD_MESSAGE_DIALOG_ID:
+			final Dialog addDialog = new Dialog(this);
+			addDialog.setContentView(R.layout.add_text_template);
+			addDialog.setTitle(R.string.find_add_template);
+			addDialog.setCancelable(true);
+			addDialog.setCanceledOnTouchOutside(true);
+			((Button)(addDialog.findViewById(R.id.addTemplateButton))).setOnClickListener(new AddTemplateListener(addDialog,true));
+			((Button)(addDialog.findViewById(R.id.addTemplateCancelButton))).setOnClickListener(new AddTemplateListener(addDialog,false));
+			d = addDialog;
+			break;
+		}
+		return d;
+	}
+    
+    class AddTemplateListener implements OnClickListener{
+    	private Dialog d;
+    	private boolean addTemplate = true;
+    	
+    	public AddTemplateListener(Dialog d, boolean addTemplate){
+    		this.d = d;
+    		this.addTemplate = addTemplate;
+    	}
+    	
+    	@Override
+		public void onClick(View v) {
+			if (!addTemplate) d.cancel();
+			else{
+				String s = ((TextView)(((View)((View)v.getParent()).getParent()).findViewById(R.id.newTemplate))).getText().toString();
+				templatesAdapter.add(s);
+				d.dismiss();
+			}
+		}
+    }
+    
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+    	boolean result = super.onCreateOptionsMenu(menu);
+    	menu.add(Menu.NONE, Menu.NONE, Menu.NONE, getString(R.string.find_add_template));
+        return result;
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+    	showDialog(ADD_MESSAGE_DIALOG_ID);
+        return super.onOptionsItemSelected(item);
+    }
+    
+    
+}
---
+++ tags/v2.0/src/com/teamPrime/sm/data/ContactData.java
@@ -0,0 +1,117 @@
+/**
+ * Copyright © 2011 Mike Depinet
+ * All rights reserved
+ * 
+ * This file is distributed as a part of the SAFEMODE application for
+ * Android devices.  SAFEMODE is distributed under Apache License 2.0
+ * which can be found in full at http://www.apache.org/licenses/LICENSE-2.0
+ */
+
+package com.teamPrime.sm.data;
+
+import java.io.Serializable;
+import java.util.Arrays;
+
+import android.provider.ContactsContract;
+
+/**
+ * ContactData holds data for a row from the Data table in
+ * the contacts database.
+ * 
+ * @author Mike Depinet
+ * @version 1.0
+ *
+ */
+public interface ContactData extends Serializable{
+	long getRawContactId();
+	long getContactId();
+	int isPrimary();
+	int isSuperPrimary();
+	int getDataVersion();
+	String getMimeType();
+	
+	Object getData(int dataNum);
+}
+
+class ContactDataGeneric implements ContactData{
+	private static final long serialVersionUID = -1521014230297655541L;
+	private String mimeType;
+	private long contactId;
+	private long rawContactId;
+	private int isPrimary;
+	private int isSuperPrimary;
+	private int dataVersion;
+	private Object[] data;
+	
+	public ContactDataGeneric(String mimeType, long contactId, long rawContactId, int isPrimary, int isSuperPrimary, int dataVersion){
+		this.mimeType = mimeType;
+		this.contactId = contactId;
+		this.rawContactId = rawContactId;
+		this.isPrimary = isPrimary;
+		this.isSuperPrimary = isSuperPrimary;
+		this.dataVersion = dataVersion;
+	}
+	public ContactDataGeneric(String mimeType, long contactId, long rawContactId, int isPrimary, int isSuperPrimary, int dataVersion, Object... data){
+		this(mimeType, contactId, rawContactId, isPrimary, isSuperPrimary, dataVersion);
+		this.data = new Object[Math.min(15, data.length)]; //Throw out anything over 15 data columns since the table only has 15
+		for (int i = 0; i<this.data.length; i++){
+			this.data[i] = data[i];
+		}
+		fixData(); //Shouldn't be necessary, but if there is bad data, our application shouldn't explode as a result
+	}
+	
+	public String getMimeType(){
+		return mimeType;
+	}
+	public long getRawContactId(){
+		return rawContactId;
+	}
+	public long getContactId(){
+		return contactId;
+	}
+	public int isPrimary(){
+		return isPrimary;
+	}
+	public int isSuperPrimary(){
+		return isSuperPrimary;
+	}
+	public int getDataVersion(){
+		return dataVersion;
+	}
+
+	public Object getData(int dataNum){
+		if (dataNum<1||dataNum>data.length) return null;
+		else return data[dataNum-1];
+	}
+	
+	private void fixData(){
+		if (getData(2) == null && getData(3) != null){
+			if (ContactsContract.CommonDataKinds.Phone.CONTENT_ITEM_TYPE.equals(mimeType)) data[1] = ContactsContract.CommonDataKinds.Phone.TYPE_CUSTOM;
+			else if (ContactsContract.CommonDataKinds.Email.CONTENT_ITEM_TYPE.equals(mimeType)) data[1] = ContactsContract.CommonDataKinds.Email.TYPE_CUSTOM;
+			else if (ContactsContract.CommonDataKinds.Im.CONTENT_ITEM_TYPE.equals(mimeType)) data[1] = ContactsContract.CommonDataKinds.Im.TYPE_CUSTOM;
+		}
+		if (getData(5) == null && getData(6) != null && ContactsContract.CommonDataKinds.Im.CONTENT_ITEM_TYPE.equals(mimeType)){
+			data[4] = ContactsContract.CommonDataKinds.Im.PROTOCOL_CUSTOM;
+		}
+	}
+
+	@Override
+	public String toString(){
+		String s = "[";
+		for (Object obj : data) s += obj.toString()+", ";
+		s = s.substring(0,s.length()-1)+"]";
+		if (ContactsContract.CommonDataKinds.Phone.CONTENT_ITEM_TYPE.equals(mimeType)) s = "PHONE: "+s;
+		else if (ContactsContract.CommonDataKinds.Email.CONTENT_ITEM_TYPE.equals(mimeType)) s = "EMAIL: "+s;
+		else if (ContactsContract.CommonDataKinds.Im.CONTENT_ITEM_TYPE.equals(mimeType)) s = "IM: "+s;
+		return s;
+	}
+	
+	@Override
+	public boolean equals(Object obj){
+		if (!(obj instanceof ContactDataGeneric)) return false;
+		ContactDataGeneric cd = (ContactDataGeneric)obj;
+		return getMimeType().equals(cd.getMimeType()) && getRawContactId() == cd.getRawContactId()
+			&& getContactId() == cd.getContactId() && isPrimary() == cd.isPrimary() && isSuperPrimary() == cd.isSuperPrimary()
+			&& getDataVersion() == cd.getDataVersion() && Arrays.equals(data, cd.data);
+	}
+}
