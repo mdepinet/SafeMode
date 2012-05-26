d17bce4b1dcad580b22c3f7887e8e470c173827f
mikedabomb000@gmail.com <mikedabomb000@gmail.com@c7e220af-e997-f780-5162-34518a5ba3d8>
2012-05-26 21:17:16 +0000

    SafeMode Lite v2.0 tag

---
+++ tags/SML v2.0/src/com/teamPrime/sml/MathStopActivity.java
@@ -0,0 +1,115 @@
+/**
+ * Copyright © 2011 Mike Depinet
+ * All rights reserved
+ * 
+ * This file is distributed as a part of the SAFEMODE application for
+ * Android devices.  SAFEMODE is distributed under Apache License 2.0
+ * which can be found in full at http://www.apache.org/licenses/LICENSE-2.0
+ */
+
+package com.teamPrime.sml;
+
+import java.util.Random;
+
+import android.app.Activity;
+import android.content.SharedPreferences;
+import android.content.pm.ActivityInfo;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+
+public class MathStopActivity extends Activity{
+	public static final int MAX_ATTEMPTS = 3;
+	
+	private TextView question;
+	private TextView answer;
+	private int corAnswer;
+	private boolean onState;
+	private int numAttempts;
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.math_stop);
+ 
+        question = (TextView) findViewById(R.id.questionStopText);
+        answer = (TextView) findViewById(R.id.answerScreenStop);
+        SharedPreferences data = getSharedPreferences("SAFEMODE", MODE_PRIVATE);
+        onState = data.getBoolean("onState", true);
+        numAttempts = data.getInt("failedAttempts", 0);
+        if (!onState) finish(); //Don't even run if SAFEMODE is off.  This should return the user to the main screen
+        if (numAttempts >= MAX_ATTEMPTS){
+        	Toast.makeText(getBaseContext(), getString(R.string.stop_attemptsExceeded), Toast.LENGTH_SHORT).show();
+        	finish();
+        }
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_NOSENSOR);
+    }
+
+    /** Called upon subsequent calls to Activity **/
+    
+    @Override
+   protected void onResume() {
+    	super.onResume();
+        int mult1 = MathUtils.genSmallRandom();
+        int mult2 = MathUtils.genLargeRandom();
+        String mathQuestion = String.format(getString(R.string.math_question)+" %d * %d?", mult1, mult2);
+    	question.setText(mathQuestion);
+    	corAnswer = mult1 * mult2;
+    }
+    
+    public void onClick(View v) {
+    	if (v.getId() == R.id.submitStopButton) {
+    		String drunkAnsString = answer.getText().toString();
+    		try {
+    			int drunkAns = Integer.parseInt(drunkAnsString);
+    			if (drunkAns == corAnswer) {
+					SharedPreferences data = getSharedPreferences("SAFEMODE", MODE_PRIVATE);
+					SharedPreferences.Editor editor = data.edit();
+					editor.putBoolean("onState", false);
+					onState = false;
+					editor.commit();
+					
+					Toast.makeText(getApplicationContext(), getString(R.string.turn_off), Toast.LENGTH_SHORT).show();
+					finish();
+    			}
+    			else {
+    				SharedPreferences.Editor editor = getSharedPreferences("SAFEMODE", MODE_PRIVATE).edit();
+    				editor.putInt("failedAttempts", ++numAttempts);
+    				editor.commit();
+    				Toast.makeText(getApplicationContext(), getString(R.string.wrong_ans) + "\n"+(MAX_ATTEMPTS-numAttempts)+" attempt"+(MAX_ATTEMPTS-numAttempts != 1 ? "s" : "")+" remaining", Toast.LENGTH_SHORT).show();
+    				finish();
+    			}
+    		}
+    		catch (NumberFormatException e) {
+    			SharedPreferences.Editor editor = getSharedPreferences("SAFEMODE", MODE_PRIVATE).edit();
+				editor.putInt("failedAttempts", ++numAttempts);
+				editor.commit();
+    			Toast.makeText(getApplicationContext(), getString(R.string.wrong_ans) + "\n"+(MAX_ATTEMPTS-numAttempts)+" attempt"+(MAX_ATTEMPTS-numAttempts != 1 ? "s" : "")+" remaining", Toast.LENGTH_SHORT).show();
+    			finish();
+    			
+    		}
+    		
+    	}
+    }
+    
+    
+    static class MathUtils {
+    	
+    	private static final int[] smallChoice = {3, 4, 6, 7, 8, 9};
+    	private static final int[] largeChoice = {12, 13, 14, 16, 17, 18, 19};
+    	private static Random r = new Random();
+    	
+    	public static int genSmallRandom() {
+    		int index = r.nextInt(smallChoice.length);
+    		return smallChoice[index];
+    	}
+    	
+    	public static int genLargeRandom() {
+    		int index = r.nextInt(largeChoice.length);
+    		return largeChoice[index];
+    	}
+
+    }
+}
